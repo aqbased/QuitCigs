c287e50f8406aebba443b0d91604276ca15e0fa3
aqbased <aqbased@hotmail.com>
2021-04-14 14:42:09 -0500

    Add and modify journal routes + axios
    I added the Show/Index/Update/Delete for the journal entry resource. I
    also did the axios calls.

--- src/api/journal_api.js
+++ src/api/journal_api.js
@@ -12,3 +12,41 @@ export const journalCreate = (journal, user) => {
     data: { journal }
   })
 }
+export const journalShow = (id, user) => {
+  console.log('made it to journalShow and here are id and user' + id + user)
+  return axios({
+    url: apiUrl + '/journal/' + id,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+export const journalIndex = (user) => {
+  return axios({
+    url: apiUrl + '/journal',
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+export const journalUpdate = (id, journal, user) => {
+  return axios({
+    url: apiUrl + '/journal/' + id,
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: { journal }
+  })
+}
+export const journalDelete = (id, user) => {
+  return axios({
+    url: apiUrl + '/journal/' + id,
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
